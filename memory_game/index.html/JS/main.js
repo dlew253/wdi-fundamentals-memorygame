2110dd3615202210965aa5571c175d66f517d76b
Dylan Lewis <dlew253@yahoo.com>
2020-01-14 01:23:06 -0800

    created game logic

--- memory_game/index.html/JS/main.js
+++ memory_game/index.html/JS/main.js
@@ -2,21 +2,28 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay=[];
-
 var cardOne = cards[0];
+
 cardsInPlay.push(cardOne);
 
+console.log("User flipped a Queen");
+
 var cardTwo = cards[2];
+
 cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+console.log("User flipped a king");
 
 if (cardsInPlay.length === 2) {
 }
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
   } else {
     alert("Sorry, try again");
-  }
+  } 
+}
+
+var flipCard = function(cardId) {
+  console.log("User flipped" + cards[cardId]
+
